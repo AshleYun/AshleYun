19fd3632ba55587fa2eaa5eec7359d9b60bd2263
AshleYun <90179048+AshleYun@users.noreply.github.com>
2021-09-10 09:18:55 +0900

    Python_2day
    문자열 다루기

---
+++ String1.py
@@ -0,0 +1,42 @@
+
+#문자열에 작은 따옴표 ' 포함시키기
+Food = "Python's"
+print (Food)
+
+#문자열에 큰 따옴표 " 포함시키기
+say = '"Python" is easy'
+print (say)
+
+#백슬라시 / + ' 또는 "
+Backslacy ='Python\'s'
+print (Backslacy)
+
+#줄바꿈 \n
+Multiline = "Life is too short\nYou need python"
+print (Multiline)
+
+#연속된 작은 따옴표 3개, 큰따옴표 3개
+Multi_1 = '''
+Life is too short
+You need python
+'''
+print (Multi_1)
+
+
+#참고사항
+#\t : 문자열 사이에 탭 간격을 줄 때
+#\\ : 문자를 \그대로 사용할 때
+
+
+#문자열 더해서 연결하기
+head = "Python"
+tail = "is fun"
+print(head+tail)
+
+#문자열 곱하기 - 이건 좀 신박함
+a = "Python"
+print(a * 2)
+
+#문자열 길이 구하기
+a = "Life is too short"
+print (len(a))
